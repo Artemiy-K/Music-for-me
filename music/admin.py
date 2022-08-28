3e7d2f92893c05302944c8dcd4959992d3d19fdf
Artemprogrmist1337 <110352581+Artemprogrmist1337@users.noreply.github.com>
2022-08-28 16:02:26 +0200

    add a music from admin

--- music/admin.py
+++ music/admin.py
@@ -2,11 +2,15 @@ from django.contrib import admin
 from .models import Music
 
 class MusicAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'created_at')
+    list_display = ('id', 'title', 'text', 'created_at')
     list_display_links = ('id', 'title')
+    search_fields = ('title', 'text')
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'title')
     list_display_links = ('id', 'title')
     search_fields = ('title',)
 # Register your models here.
+
+
+admin.site.register(Music, MusicAdmin)
